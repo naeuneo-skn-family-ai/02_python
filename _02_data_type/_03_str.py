01048152347de9e8f74f3c78f67fc7c75f113c70
naeuneo <nangni30@naver.com>
2026-06-01 14:56:36 +0900

    20260601_list(기초, mutable, 2차원, slicing, iterable)

--- _02_data_type/_03_str.py
+++ _02_data_type/_03_str.py
@@ -19,7 +19,7 @@ print("""
 print("""앞/뒤 엔터 없이 작성하려면
 따옴표와 문자열을 딱 붙여서 작성""")
 
-#---------------------------------------
+# ---------------------------------------
 # str연산
 # 1. 문자열 + 문자열 = 이어쓰기
 print('--- 문자열 더하기 연산 ---')
@@ -33,14 +33,14 @@ print('😎' * 10)
 # 빼기 나누기 연산은 불가
 # print(a - b)
 
-#---------------------------------------
+# ---------------------------------------
 # len(객체) 함수: 파이썬 객체 길이 반환 -> 함수
 # 파이썬 객체 : str, list, tuple, dict, set 등
 print('--- len() ---')
 text = '오늘 점심은 뭘 먹을까'
 print(text, len(text))
 
-#---------------------------------------
+# ---------------------------------------
 # str 메서드 (str api)
 # (참고) 함수, 매서드 == 기능 (실행 후 결과 반환)
 
@@ -49,8 +49,7 @@ print(text, len(text))
 # - str 내에서 old에 해당하는 문자를 new로 치환(변경)
 print('--- str.replace() ---')
 today = '2026-06-01'
-print(today,'to', today.replace('-', '/'))
-
+print(today, 'to', today.replace('-', '/'))
 
 # str.strip([str])
 # - 문자열 좌우 [str] 제거
@@ -61,29 +60,98 @@ some = '             하하하      '
 print('[' + some + ']')
 print('[' + some.strip() + ']')
 
-
 # 대소문자 관련 str 메서드
 print('--- 대소문자 관련 str 메서드 ---')
 origin_str = 'hELLO wORLD!'
 
-print(origin_str.upper())         # HELLO WORLD! (대문자)
-print(origin_str.lower())         # hello world! (소문자)
-print(origin_str.capitalize())    # Hello world! (가장 앞 글자만 대문자)
-print(origin_str.swapcase())      # Hello World! (대문자 소문자 바꾸기)
-print(origin_str.title())         # Hello World! (단어 앞 글자만 대문자)
+print(origin_str.upper())  # HELLO WORLD! (대문자)
+print(origin_str.lower())  # hello world! (소문자)
+print(origin_str.capitalize())  # Hello world! (가장 앞 글자만 대문자)
+print(origin_str.swapcase())  # Hello World! (대문자 소문자 바꾸기)
+print(origin_str.title())  # Hello World! (단어 앞 글자만 대문자)
 
 # 문자열 포맷팅
 
 # 1. % 포맷팅
 print('--- % 포맷팅 ---')
 x = 10
-print("x is %d" %x) # x is 10
+print("x is %d" % x)  # x is 10
 
 # 2. str.format()
 x = 10
 y = 1.23
-print('{} + {} = {}'.format(x, y, x+y))
+print('{} + {} = {}'.format(x, y, x + y))
 
 # 3. f-string (python 3.6)
 print('--- f-string ---')
-print(f"{x} + {y} = {x+y}")
+print(f"{x} + {y} = {x + y}")
+
+#---------------------------------------
+# 문자열 인덱싱/슬라이싱
+# - 파이썬 문자열(str)은 text sequence 형태를 갖는다
+# - sequence :  순차적인, 순서가 있는 데이터 구조
+# - index : 순서 (base-index == 0)
+# - 마지막 index == str 길이 -1
+
+print('--- 문자열 인덱싱 ---')
+x = 'Monday'
+print('x의 길이', len(x))  # 길이 6, 인덱스 0, 1, 2, 3, 4, 5
+print(x[0])  # [] == 배열, [0] == str 배열 중 0번째 인덱스
+print(x[1])
+print(x[2])
+print(x[3])
+print(x[4])
+print(x[5])
+# print(x[6]) # 초과, IndexError : string index out of range
+
+# 역 인덱스 : str을 거꾸로 탐색
+print('--- 역 인덱스 ---')
+print(x[-1], x[-2], x[-3], x[-4], x[-5], x[-6])
+
+# str 슬라이싱 : 문자열 일부를 잘라서 가져오는 방법
+# 작성법 : str[start:stop:step]
+# - start : 시작 인덱스
+# - stop : 종료 인덱스 (미포함)
+# - step : 건너 뛸 개수 (생략 시 기본 값 1)
+print('--- str slicing ---')
+
+text = 'hello world'
+print('text :', text)
+print('len(text) :', len(text))
+
+print('text[0:5:1] :', text[0:5:1])
+print('text[0:5] :', text[0:5])
+print('text[:5] :', text[:5])
+
+print('text[6:11] :', text[6:11])
+print('text[6:len(text)] :', text[6:len(text)])
+print('text[6:] :', text[6:]) # 6 시작하고 끝까지
+print("text[:] :", text[:]) # 0 부터 끝까지
+
+print("text[0:11:2] :", text[0:11:2]) # 0, 2, 4, 8, 10
+print("text[::2] }", text[::2]) # 0, 2, 4, 8, 10
+print("text[::-1] :", text[::-1]) # 문자열 뒤집기
+
+
+# 문자열 불변 타입 (immutable)
+# - str은 한 번 메모리에 값이 저장되면 수정할 수 없다.
+# id(변수명) : 변수에 저장된 값의 주소(위치)를 반환
+
+print('--- 문자열 불변 타입 ---')
+s = 'python' # s에는 'python' str 메모리 주소가 저장됨
+print("s :", s) # s에 저장된 주소를 찾아가서 'python' str을 참조
+print("변경 전 s :", id(s))
+
+s = s + ' hello'
+print("s :", s)
+print('변경 후 s :', id(s))
+
+
+#---------------------------------------
+# in 연산자
+# - 특정 값이 포함되어 있는지 검사
+# - 결과는 bool 형태
+print('--- in 연산자 ---')
+txt = "김밥, 라면, 어묵, 떡볶이"
+print('라면' in txt)
+print('돈까스' in txt)
---
+++ _02_data_type/_04_none.py
@@ -0,0 +1,11 @@
+# None Type
+# - 값이 없음을 뜻하는 자료형
+
+a = None
+print(a, type(a))
+
+# 변수에 저장된 값을 제거
+s = 'hello'
+print(s, type(s))
+s = None
+print(s, type(s))
